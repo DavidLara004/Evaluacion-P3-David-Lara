ce6a0fc1ffe306ba58188f2fc1fbcb7dfbddf5e0
David Lara <david.lara.moreira@udla.edu.ec>
2023-07-10 15:18:38 -0500

    Primer intento de lectura de archivo sin documentar

--- examentresp.c
+++ examentresp.c
@@ -2,8 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 int main(){
+char delimitador[]={";"};
+char caracter[6];
+struct archivo{
+int nombre[50];
+float promedio;
+};
+FILE *archivo;
+archivo=fopen("alumnos.txt", "r+");
+while (!feof(archivo))
+    {
+        caracter[6] = fgetc(archivo);
+        putchar(caracter);
+        printf("%c", caracter);
+    }
 
-
+fclose(archivo);
 return 0;
 }
 
